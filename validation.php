08d2e298bed64ce6c93b52dab54539771dc849b8
Christophe Goncalves Da Fonseca <Christophe.goncalvesdafonseca@gmail.com>
2016-09-12 18:34:44 +0200

    Ajout des balises name au formulaire de Login. #1

--- validation.php
+++ validation.php
@@ -3,12 +3,23 @@ require('config/config.php');
 require('model/functions.fn.php');
 session_start();
 
-if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && 
+if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) &&
 	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
 
-	// TODO
-
-}else{ 
+		$pseudo = htmlspecialchars($_POST["username"]);
+		$password = htmlspecialchars($_POST["password"]);
+		$email = htmlspecialchars($_POST["email"]);
+		$request = $db->prepare("INSERT INTO users (username, password, email, created_at)
+	  VALUES (:pseudo, :password, :email, NOW())");
+    $request->execute(
+    array(
+      "pseudo" => $pseudo,
+      "password" => $password,
+			"email" => $email
+    )
+    );
+		header('Location: login.php');
+}else{
 	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
 	header('Location: register.php');
 }
