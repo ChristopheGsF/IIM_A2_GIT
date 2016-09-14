386133d748253f4fe128fab97af868c3a09ce2b6
abassi <Nadjime.mohamed@gmail.com>
2016-09-14 18:08:14 +0200

    Finalisation de l'inscription. Fix #2

--- validation.php
+++ validation.php
@@ -1,24 +1,28 @@
-<?php
+<?php session_start();
+
 require('config/config.php');
 require('model/functions.fn.php');
-session_start();
 
 if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) &&
 	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
 
-		$pseudo = htmlspecialchars($_POST["username"]);
-		$password = htmlspecialchars($_POST["password"]);
-		$email = htmlspecialchars($_POST["email"]);
-		$request = $db->prepare("INSERT INTO users (username, password, email, created_at)
-	  VALUES (:pseudo, :password, :email, NOW())");
-    $request->execute(
-    array(
-      "pseudo" => $pseudo,
-      "password" => $password,
-			"email" => $email
-    )
-    );
-		header('Location: login.php');
+if(isEmailAvailable($db, $_POST['email']) && isUsernameAvailable($db, $_POST['username']))
+{
+	userRegistration($db, $_POST['username'], $_POST['email'], $_POST['password']);
+	header('Location: login.php');
+}
+else 
+{
+
+	$_SESSION['message'] = 'ici';
+header('Location: register.php');
+	//if(isEmailAvailable($db, $_POST['email'] == false)
+	//	$error = 'Email indisponible';
+	//if (isUsernameAvailable($db, $_POST['username']) == false) {
+//		$error = 'Username indisponible';
+//	}
+}
+
 }else{
 	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
 	header('Location: register.php');
